4f9fec84d15e5164db287636fff28590e9271a99
Yui Jensen <kyanyui67@gmail.com>
2025-12-28 23:44:07 +0100

    Modified RootSection and added some codes in about section

--- my-app/app/about/page.tsx
+++ my-app/app/about/page.tsx
@@ -1,25 +1,27 @@
 import Image from 'next/image';
 import ProfileAboutImage from '../../public/about-profile.jpg';
-import Line from '../../public/Line 33.png';
 import RootSection from '@/components/RootSection';
 export default function About() {
   return (
     <>
       <RootSection title='ABOUT'>
-        <Image
-          src={Line}
-          alt='Underline of text'
-          width={800}
-          className='mb-20'
-        />
-
-        <div className=' m-auto'>
+        <div className='flex flex-col items-center text-center gap-8'>
           <Image
             src={ProfileAboutImage}
             alt='Profile of myself with buildings'
             width={1000}
             height={500}
+            priority={true}
           />
+          <h3 className='text-2xl text-(--color-letter)'>ヤンセン　唯</h3>
+          <p className='text-(--color-letter) 1rem  max-w-3xl px-4'>
+            ヤンセン　唯 1996年生まれ、広島出身。
+            高校卒業後にアパレル業界へ就職し、その後ワーキングホリデー制度を利用してカナダに1年半滞在しました。
+            帰国後は英語を活かしてホテルのフロント業務に従事し、再びワーキングホリデーでデンマークへ渡航。そこで現在の夫と出会ったことをきっかけに、2022年よりスウェーデンに拠点を移しました。
+            デンマークの企業でマーケティングのインターンとして働く中で、
+            SNS向けの画像制作に携わったことをきっかけに、UX/UIデザインやフロントエンド開発への興味が深まりました。そこから独学で学び始め、2024年には現地の職業学校で約1年間、フロントエンドとバックエンドの基礎を幅広く学び、2025年1月に修了しました。
+            現在はWebデザイナーを目指し、メンターのサポートを受けながらデザイン力と技術力を日々磨いています。
+          </p>
         </div>
       </RootSection>
     </>
--- my-app/components/RootSection.tsx
+++ my-app/components/RootSection.tsx
@@ -1,3 +1,8 @@
+'use client';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Line from '../public/Line.png';
+const MotionImage = motion.create(Image);
 export default function RootSection({
   children,
   title = '',
@@ -7,10 +12,27 @@ export default function RootSection({
 }) {
   return (
     <>
-      <section className='flex flex-col mt-30'>
-        <h1 className='text-5xl text-amber-500'>{title}</h1>
-        {children}
-      </section>
+      {title && (
+        <motion.section
+          className='flex flex-col mt-30 mb-20'
+          initial={title ? { opacity: 0, y: 50 } : {}}
+          whileInView={title ? { opacity: 1, y: 0 } : {}}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5 }}
+        >
+          <h2 className='text-5xl text-(--color-accent) ml-20'>{title}</h2>
+          <MotionImage
+            src={Line}
+            alt='Underline of text'
+            width={800}
+            className='mb-20 ml-20'
+            initial={title ? { opacity: 0, x: -500 } : {}}
+            whileInView={title ? { opacity: 1, x: 0 } : {}}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          />
+          {children}
+        </motion.section>
+      )}
     </>
   );
 }
