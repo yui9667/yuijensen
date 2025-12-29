74a0c097cbab31e9027873370f929df031403338
Yui Jensen <kyanyui67@gmail.com>
2025-12-29 17:31:34 +0100

    Added some interest pictures and added codes in about section

--- my-app/app/about/page.tsx
+++ my-app/app/about/page.tsx
@@ -1,7 +1,93 @@
 import Image from 'next/image';
-import ProfileAboutImage from '../../public/about-profile.jpg';
+import ProfileAboutImage from '../../public/images/about-profile.jpg';
 import RootSection from '@/components/RootSection';
 export default function About() {
+  const photos = [
+    {
+      src: '/images/photo1.jpg',
+      alt: 'Picture of drums',
+      label: '学生の頃から音楽が好きで、　家でドラムを叩いています',
+    },
+    {
+      src: '/images/photo2.jpg',
+      alt: 'Picture of dance',
+      label: '最近はダンスにも興味があり、週に一度クラスに参加しています',
+    },
+    {
+      src: '/images/photo3.jpg',
+      alt: 'Picture of baking',
+      label: '日本のスイーツは手に入らないので、自分でよくを作ります',
+    },
+    {
+      src: '/images/photo4.jpg',
+      alt: 'Picture of game controller',
+      label: 'Playstationでゲームをしてます',
+    },
+  ];
+
+  const career = [
+    { date: '2015.03', event: '高校卒業' },
+    {
+      date: '2015.04 - 2017.01',
+      event: '株式会社アダストリア　LOWRYS FARM　 広島パルコ店　販売員',
+    },
+    {
+      date: '2017.01 - 2018.08',
+      event: 'ワーキングホリデーのためカナダへ渡航',
+    },
+    {
+      date: '2018.09 -2019.12',
+      event:
+        'ソラーレ　ホテルズ　アンドリゾーツ株式会社 チサンホテル広島　フロントスタッフ',
+    },
+    {
+      date: '2020.01 - 2021.01',
+      event: 'ワーキングホリデーのためデンマークへ渡航',
+    },
+    {
+      date: '2021.02 - 2021.10',
+      event:
+        'ソラーレ　ホテルズ　アンドリゾーツ株式会社 チサンホテル広島　フロントスタッフ',
+    },
+    {
+      date: '2021.09 - 2021.10',
+      event: 'ProjectNord (インターン) インフルエンサーマーケティング',
+    },
+    {
+      date: '2024.01 - 2024.06',
+      event: 'Sundsgårdens Folkhögskola フロントエンドを学ぶ',
+    },
+    {
+      date: '2024.08 - 2025.01',
+      event: 'Sundsgårdens Folkhögskola バックエンドを学ぶ',
+    },
+  ];
+  const skills = [
+    { title: 'デザイン', items: ['Figma'] },
+    { title: '言語', items: ['HTML', 'CSS', 'JavaScript', 'TypeScript'] },
+    { title: 'フレームワーク', items: ['React'] },
+    {
+      title: 'CSS フレームワーク',
+      items: ['Tailwind CSS', 'Bootstrap', 'Sass'],
+    },
+  ];
+  const strengths = [
+    {
+      title: '自走力',
+      description:
+        '2年前からデザインとコードの学習を始め、最初の1年間は独学で、さらにその後は1年間スウェーデンの職業学校に行きました。学ぶたびに理解が深まる達成感を味わうのが好きですし、「今日よりも明日、もっと成長したい」という気持ちが、私の学びを継続させています。',
+    },
+    {
+      title: '向上心',
+      description:
+        '高校を卒業してからのこの10年間、常に何かを学び続けてきました。英語も独学で勉強を重ね、数年前にはIELTS6を取得。現在は少しずつですがデンマーク語の学習にも取り組んでいます。また、より目に留まるデザインを作れるよう、デザインの勉強も日々続けています。',
+    },
+    {
+      title: '傾聴力',
+      description:
+        '他人の意見や感情を尊重し、理解する力を大切にしています。常に相手の立場に立って話を聞くことで、信頼関係を築き、より良い結果を導くことができます。',
+    },
+  ];
   return (
     <>
       <RootSection title='ABOUT'>
@@ -13,15 +99,78 @@ export default function About() {
             height={500}
             priority={true}
           />
-          <h3 className='text-2xl text-(--color-letter)'>ヤンセン　唯</h3>
-          <p className='text-(--color-letter) 1rem  max-w-3xl px-4'>
-            ヤンセン　唯 1996年生まれ、広島出身。
+        </div>
+        <div className='flex flex-col justify-center'>
+          <h3 className='text-2xl mt-10 mb-8'>ヤンセン　唯</h3>
+          <p className=' mb-8 '>1996年生まれ、広島出身。</p>
+          <p className=' max-w-5xl m-2 leading-6 text-sm '>
             高校卒業後にアパレル業界へ就職し、その後ワーキングホリデー制度を利用してカナダに1年半滞在しました。
             帰国後は英語を活かしてホテルのフロント業務に従事し、再びワーキングホリデーでデンマークへ渡航。そこで現在の夫と出会ったことをきっかけに、2022年よりスウェーデンに拠点を移しました。
             デンマークの企業でマーケティングのインターンとして働く中で、
             SNS向けの画像制作に携わったことをきっかけに、UX/UIデザインやフロントエンド開発への興味が深まりました。そこから独学で学び始め、2024年には現地の職業学校で約1年間、フロントエンドとバックエンドの基礎を幅広く学び、2025年1月に修了しました。
             現在はWebデザイナーを目指し、メンターのサポートを受けながらデザイン力と技術力を日々磨いています。
           </p>
+        </div>
+      </RootSection>
+      <RootSection title='CAREER'>
+        <div className='max-w-7xl flex flex-col items-end'>
+          {career.map((item, index) => (
+            <div key={index} className='w-full max-w-130 px-4 ml-auto mb-5'>
+              <p>{item.date}</p>
+              <p>{item.event}</p>
+              <div className='w-full border-b-2 border-current mt-2' />
+            </div>
+          ))}
+        </div>
+      </RootSection>
+      <RootSection title='SKILLS'>
+        <div className='max-w-7xl flex flex-col items-end'>
+          {skills.map((skill, index) => (
+            <div key={index} className='w-full max-w-130 px-4 ml-auto mb-5'>
+              <p className='font-bold'>{skill.title}</p>
+              <ul className='list-disc list-inside'>
+                {skill.items.map((item, itemIndex) => (
+                  <li key={itemIndex}>{item}</li>
+                ))}
+              </ul>
+              <div className='w-full border-b-2 border-current mt-2' />
+            </div>
+          ))}
+        </div>
+      </RootSection>
+      <RootSection title='STRENGTHS'>
+        <div className='max-w-7xl flex flex-col items-end'>
+          {strengths.map((strength, index) => (
+            <div key={index} className='w-full max-w-130 px-4 ml-auto mb-5'>
+              <p className='font-bold'>{strength.title}</p>
+              <p>{strength.description}</p>
+              <div className='w-full border-b-2 border-current mt-2' />
+            </div>
+          ))}
+        </div>
+      </RootSection>
+      <RootSection title='INTEREST'>
+        <div className='max-w-150 mx-auto'>
+          <div className='grid grid-cols-1 mx-auto gap-4 md:grid-cols-'>
+            {photos.map((photo, item) => (
+              <div key={item} className='relative group w-full h-75'>
+                <Image
+                  src={photo.src}
+                  alt={photo.alt}
+                  width={300}
+                  height={300}
+                  className='object-cover w-full h-full duration-300 '
+                />
+
+                <div className='absolute inset-0 bg-(--color-letter)/30 group-hover:bg-(--color-letter) transition z-10' />
+                <div className='text-(--color-contactBtn-interest)  absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-70 transition z-20 p-4'>
+                  <span className='opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 text-center'>
+                    {photo.label}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </RootSection>
     </>
--- my-app/components/RootSection.tsx
+++ my-app/components/RootSection.tsx
@@ -1,33 +1,36 @@
 'use client';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import Line from '../public/Line.png';
+import Line from '../public/images/line.png';
 const MotionImage = motion.create(Image);
 export default function RootSection({
   children,
   title = '',
 }: {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   title?: string;
 }) {
   return (
     <>
       {title && (
         <motion.section
-          className='flex flex-col mt-30 mb-20'
+          className='flex flex-col mt-30 mb-20 mx-auto max-w-1400 px-4'
           initial={title ? { opacity: 0, y: 50 } : {}}
           whileInView={title ? { opacity: 1, y: 0 } : {}}
           viewport={{ once: true }}
           transition={{ duration: 0.5 }}
         >
-          <h2 className='text-5xl text-(--color-accent) ml-20'>{title}</h2>
+          <h2 className='text-3xl sm:text-4xl md:text-5xl text-(--color-accent) '>
+            {title}
+          </h2>
           <MotionImage
             src={Line}
             alt='Underline of text'
             width={800}
-            className='mb-20 ml-20'
+            className='mb-20 '
             initial={title ? { opacity: 0, x: -500 } : {}}
             whileInView={title ? { opacity: 1, x: 0 } : {}}
+            viewport={{ once: true }}
             transition={{ duration: 0.8, delay: 0.2 }}
           />
           {children}
